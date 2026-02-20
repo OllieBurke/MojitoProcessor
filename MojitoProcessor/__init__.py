3f9dbed39c9fbe319447942d2ffd971ffd59b8f1
Ollie Burke <ollie.burke@glasgow.ac.uk>
2026-02-20 15:47:03 +0000

    updated

--- MojitoProcessor/__init__.py
+++ MojitoProcessor/__init__.py
@@ -1,33 +1,35 @@
-"""MojitoUtils - LISA Mojito L1 data loading and analysis utilities"""
+"""MojitoProcessor - LISA Mojito L1 data loading and analysis utilities"""
 
+from .__version__ import __version__
 from .mojito_loader import (
-    load_mojito_l1, 
+    MojitoData,
+    OrbitData,
+    investigate_mojito_file_structure,
+    load_mojito_l1,
     load_orbits,
     truncate_mojito_data,
     truncate_orbit_data,
-    investigate_mojito_file_structure, 
-    MojitoData,
-    OrbitData
 )
 from .SigProcessing import (
     SignalProcessor,
-    bandpass_filter,
     apply_window,
+    bandpass_filter,
     downsample,
     process_pipeline,
 )
 
 __all__ = [
-    'load_mojito_l1',
-    'load_orbits',
-    'truncate_mojito_data',
-    'truncate_orbit_data',
-    'investigate_mojito_file_structure', 
-    'MojitoData',
-    'OrbitData',
-    'SignalProcessor',
-    'bandpass_filter',
-    'apply_window',
-    'downsample',
-    'process_pipeline',
+    "__version__",
+    "load_mojito_l1",
+    "load_orbits",
+    "truncate_mojito_data",
+    "truncate_orbit_data",
+    "investigate_mojito_file_structure",
+    "MojitoData",
+    "OrbitData",
+    "SignalProcessor",
+    "bandpass_filter",
+    "apply_window",
+    "downsample",
+    "process_pipeline",
 ]
