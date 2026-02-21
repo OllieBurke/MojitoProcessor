7c5ba6000807f4ddbbac102738ad221ac68f6390
Ollie Burke <ollie.burke@glasgow.ac.uk>
2026-02-21 01:08:54 +0000

    bumps in version

--- docs/conf.py
+++ docs/conf.py
@@ -11,7 +11,7 @@ sys.path.insert(0, str(REPO_ROOT))
 # -- Project information -----------------------------------------------------
 project = "MojitoProcessor"
 author = "Ollie Burke"
-release = "0.1.0"
+release = "0.2.1"
 
 # -- General configuration ---------------------------------------------------
 extensions = [
